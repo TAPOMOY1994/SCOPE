6498ab1cfeee650a08cf672b441edbf143b7f8b3
Tapomoy Das <160303114+TAPOMOY1994@users.noreply.github.com>
2024-04-04 11:26:41 +0530

    Add files via upload

---
+++ SCOPE.js
@@ -0,0 +1,41 @@
+let a=16;
+
+
+function x(){
+
+  console.log(a);
+}
+
+
+x();
+
+console.log(a);
+
+
+
+
+
+
+function y(){
+
+    var b=8;
+    console.log(b);
+}
+
+y();
+console.log(b);
+
+
+
+
+
+let c=4;
+
+if(c===4){
+
+    let d=6;
+    console.log(c);
+
+}
+
+ console.log(d);
